484e0ee5d074bab74eb9a82d27a963c5941a08b3
hykid1189 <hykid1189@gmail.com>
2025-07-24 15:54:47 +0900

    fix port

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // 정적 파일 서빙 (html, js, css 등)
 app.use(express.static(path.join(__dirname)));
 
-app.listen(port, () => {
-  console.log(`🚀 Server is now running`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server is now running on port ${port}`);
 });
